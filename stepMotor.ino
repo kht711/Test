20fc85afddf1b216e373b88a1c25c6ecaa9987df
kht711 <42952322+kht711@users.noreply.github.com>
2019-09-27 03:36:47 +0900

    Update stepMotor.ino

--- stepMotor.ino
+++ stepMotor.ino
@@ -3,23 +3,29 @@
 //2048->360 deg, 1024->180 deg
 const int steps = 2048;
 
-//밀리초 기준
-unsigned int times = 2 * 3600 * 1000;
+//밀리초 기준 (UL은 unsigned int형의 숫자라는 선언.)
+unsigned long times = (2 * 3600 * 1000UL);
 
+/* IN1->pin8
+ * IN2->pin9
+ * IN3->pin10
+ * IN4->pin11
+ */
 Stepper myStepper(steps, 11, 9, 10, 8);
 
 void setup() {
   /*
-  모터 스텝을 2048로 설정했을 때
-  안정적으로 동작하는 최대 속도는 17.
+   * step() 함수를 실행할 때 스피드 설정
    */
   myStepper.setSpeed(15);  //15 RPM
 }
 
 void loop() {
-  myStepper.step(steps);
+  // 반시계방향 360도 * 2회
+  myStepper.step(steps * 2);
   delay(times);
 
+  // 시계방향 360도 * 1회
   myStepper.step(-steps);
   delay(times);
 }
